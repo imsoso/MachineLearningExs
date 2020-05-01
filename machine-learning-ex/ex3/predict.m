2ed543c51f219be08e1b59b11db8a3a852a11107
imsoso <husosong@163.com>
2020-05-01 10:57:33 +0800

    added a bias unit to X

--- machine-learning-ex/ex3/predict.m
+++ machine-learning-ex/ex3/predict.m
@@ -20,6 +20,7 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+a1Bias = [ones(m, 1) X];
 
 
 

701aa06b46645e689413b046ab1e6489420032c3
imsoso <husosong@163.com>
2020-05-12 16:15:45 +0800

    Calculate distance from x_i to centroid_k

--- machine-learning-ex/ex7/findClosestCentroids.m
+++ machine-learning-ex/ex7/findClosestCentroids.m
@@ -20,6 +20,14 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
+m = size(X, 1);
+
+for i = 1:m
+  for k = 1:K
+    distance = X(i, :) - centroids(k, :);
+    distance_square = distance * distance';
+  end
+end
 
 
 

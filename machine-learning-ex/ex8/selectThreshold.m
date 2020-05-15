776b48e0493b79fa3c863889e3988f749d1202bf
imsoso <husosong@163.com>
2020-05-15 21:56:51 +0800

    calculate preditions on cross validation set

--- machine-learning-ex/ex8/selectThreshold.m
+++ machine-learning-ex/ex8/selectThreshold.m
@@ -22,6 +22,7 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
+cvPredictions = (pval < epsilon)
 
 
 
